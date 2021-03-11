458e2c5be68158e8c880aaa67d7efd58f89558d0
Gaurav Goswami <gaurav.goswami2@ibm.com>
2021-03-12 01:59:05 +0530

    server added

--- crawler.py
+++ crawler.py
@@ -1,24 +1,20 @@
+import json
+import sys
+import os
+
+import requests
 from requests_html import HTMLSession
+from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
-from selenium import webdriver
-import json
-import os
-import threading
-import time
-import requests
+
 
 class Crawler:
 
-    CYAN = '\033[96m'
-    GREEN = '\033[92m'
-    YELLOW = '\033[33m'
-    RED = '\033[91m'
-    WHITE = '\033[0m'
-    rejections = ['javascript:void(0)', '#', '/./', '/', 'javascript:void(0);', '/#']
-    worker_count = 1
+    rejections = ['javascript:void(0)', '#', '/./',
+                  '/', 'javascript:void(0);', '/#']
 
-    def __init__(self, host: str, rejections: list, targets: list) -> None:
+    def __init__(self, host: str, rejections: list, targets: list, logging: int) -> None:
         self.link_collection = []
         self.crawled = []
         self.host = host
@@ -28,7 +24,18 @@ class Crawler:
         self.host_crawled = False
         self.download_folder = self.create_download_folder()
         self.browser_options = self.set_browser_options()
+        self.logging = False
+        if int(logging)>0:
+            self.logging = True
+        self.result = {
+            'logs': [],
+            'downloadDir': self.download_folder,
+            'errors': []
+        }
+
+    def start_crawling(self):
         self.crawl_page(self.host)
+        print(json.dumps(self.result))
 
     def create_download_folder(self) -> None:
         cur_dir = os.getcwd()
@@ -36,13 +43,14 @@ class Crawler:
         if not os.path.exists(path):
             os.mkdir(path)
         return path
-        
+
     def set_browser_options(self) -> None:
         browser_options = Options()
         browser_options.add_argument("--headless")
         browser_options.add_argument("--no-sandbox")
         browser_options.add_argument("--disable-dev-shm-usage")
-        browser_prefs = {"profile.managed_default_content_settings.images": 2, "download.default_directory": self.download_folder}
+        browser_prefs = {"profile.managed_default_content_settings.images": 2,
+                         "download.default_directory": self.download_folder}
         browser_options.experimental_options["prefs"] = browser_prefs
         return browser_options
 
@@ -53,20 +61,33 @@ class Crawler:
         return False
 
     def meaningful_page(self, url: str) -> bool:
-        return (url not in self.crawled) and (url+'/' not in self.crawled) and (url+'/#' not in self.crawled) and (url+'#' not in self.crawled) and (url not in self.rejections) and (url.startswith('/') or (self.host in url) and (not len(url.split(".")) > 3))
-    
+        extension = url.split('/')[-1].split('.')[-1]
+        if extension in self.targets:
+            self.result['logs'].append(f"downloading {url} explicitly")
+            self.download_explicitly(url)
+            return False
+        return (url not in self.crawled) and (url+'/' not in self.crawled) and (url+'/#' not in self.crawled) and (url+'#' not in self.crawled) and (url not in self.rejections) and (url.startswith('/') or (self.host in url))
+
     def has_possible_download_buttons(self, url: str) -> bool:
         driver = webdriver.Chrome(options=self.browser_options)
         driver.get(url)
         buttons = driver.find_elements_by_css_selector("button")
         for button in buttons:
-            if 'download' in button.get_attribute('class'):
-                print(f"{self.GREEN}clicking possible download button")
-                ActionChains(driver).move_to_element(button).click(button).perform()
+            if 'download' in button.get_attribute('class') or 'download' in button.get_attribute('innerHTML') or 'Download' in button.get_attribute('innerHTML'):
+                if self.logging:
+                    self.result['logs'].append(f"clicking possible download button")
+                ActionChains(driver).move_to_element(
+                    button).click(button).perform()
         driver.close()
         driver.quit()
 
     def download_explicitly(self, url: str) -> None:
+        if url.startswith('/./'):
+            url = "{0}/{1}".format(self.host, url[3:])
+        elif url.startswith('./'):
+            url = "{0}/{1}".format(self.host, url[2:])
+        elif url.startswith('/'):
+            url = "{0}/{1}".format(self.host, url[1:])
         fileName = url.split('/')[-1]
         resp = requests.get(url)
         with open('{0}/{1}'.format(self.download_folder, fileName), 'wb') as file:
@@ -86,28 +107,28 @@ class Crawler:
                     url = "{0}/{1}".format(self.host, url[2:])
                 elif url.startswith('/'):
                     url = "{0}/{1}".format(self.host, url[1:])
-                print('{0}crawling {1}'.format(self.CYAN, url))
+                if self.logging:
+                    self.result['logs'].append(f'crawling {url}')
                 self.has_possible_download_buttons(url)
                 r = self.session.get(url)
-                r.html.render(sleep=1, keep_page=True, scrolldown=1, timeout=30)
+                r.html.render(sleep=1, keep_page=True,
+                              scrolldown=1, timeout=3000)
                 anchors = r.html.xpath('//a/@href')
                 for anchor in anchors:
                     self.crawl_page(anchor)
-            elif self.is_target(url):
-                print('{0}downloading file explicitly'.format(self.GREEN))
-                self.download_explicitly(url)
+            # elif self.is_target(url):
+            #     print('{0}downloading file explicitly'.format(self.GREEN))
+            #     self.download_explicitly(url)
             else:
-                print('{0}rejecting {1}'.format(self.YELLOW, url))
+                if self.logging:
+                    self.result['logs'].append(f'rejecting {url}')
         except Exception as e:
-            print('{0}error occurred: {1}'.format(self.RED, e))
+            if self.logging:
+                self.result['errors'].append(f'error occurred: {e}')
 
-start = time.time()
-targets = os.getenv('TARGETS', 'json,yaml,yml')
-rejections = os.getenv('REJECTIONS', '#,/,/#')
-host = os.getenv('HOST', 'https://bankofapis.com')
-if host.endswith('/'):
-    host = host[:len(host)-1]
-crawler = Crawler(host, rejections.split(','), targets.split(','))
-end = time.time()
-time = end-start
-print("finished within {:.2f} s".format(time))
+host = sys.argv[1]
+rejections = sys.argv[2]
+targets = sys.argv[3]
+logging = sys.argv[4]
+crawler = Crawler(host, rejections.split(','), targets.split(','), logging)
+crawler.start_crawling()
---
+++ server.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const cors = require('cors');
+const { spawn } = require('child_process');
+const glob = require("glob");
+const fs = require('fs');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+readFile = (file) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(file, (err, data) => {
+            if(!err){
+                resolve(data);
+            } else {
+                reject(err);
+            }
+        });
+    });
+}
+
+collectFiles = (dir, targetsStr) => {
+    return new Promise(async resolve => {
+        let result = []
+        let targets = targetsStr.split(',')
+        for(var i = 0; i < targets.length; i++) {
+            let files = glob.sync(`${dir}/*.${targets[i]}`);
+            for(var j = 0; j < files.length; j++) {
+                let def = await readFile(files[j]);
+                result.push({
+                    'name': files[j].split('/').slice(-1)[0],
+                    'def': def.toString()
+                });
+            }
+        }
+        resolve(result);
+    });
+}
+
+// app.get('/cr', (req, res) => {
+//     collectFiles('/Users/gauravgoswami/Desktop/Projects/apiscraper/selenium_crawler/downloads/ibanfox.com', 'json,yaml,yml').then(f => {
+//         res.send(f);
+//     });
+// })
+
+app.get('/', (req, res) => {
+    let result = {
+        'apis': '',
+        'response': '',
+        'errors': ''
+    }
+    let resp = spawn('python', ['./crawler.py', `${req.query['host']}`, `${req.query['rejections']}`, `${req.query['targets']}`, `${req.query['logging']}`]);
+    resp.stdout.on('data', (data) => {
+        result.response += data.toString();
+    });
+    resp.stderr.on('data', (err) => {
+        result.errors += err.toString();
+    });
+    resp.on('close', (_) => {
+        let data = JSON.parse(result.response);
+        collectFiles(data.downloadDir, req.query['targets']).then(response => {
+            res.statusCode = 200;
+            result.apis = response;
+            res.send(result);
+        });
+    });
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log('server started!');
+});
