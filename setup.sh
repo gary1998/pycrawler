009028d7656aac262f8dedd2d7b1d0c84078c980
Gaurav Goswami <gaurav.goswami2@ibm.com>
2021-03-12 12:24:02 +0530

    install chromium in buildback

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
-apt-get install -y chromium-browser
 mkdir /app/downloads
 pip install --upgrade pip
 pip install -r requirements.txt
 npm install
+export CHROMEDRIVER_PATH = /app/.chromedriver/bin/chromedriver
+export GOOGLE_CHROME_BIN = /app/.apt/usr/bin/google-chrome
 export PORT=$PORT
 node ./server.js
